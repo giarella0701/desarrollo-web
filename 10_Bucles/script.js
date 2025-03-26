e4403276c1e07c98b8fd3484e33bbc9283913347
leslieVVH <leslievillagra@liceovvh.cl>
2025-03-26 09:29:50 -0300

    ok

---
+++ 10_Bucles/script.js
@@ -0,0 +1,21 @@
+console.log("Funcionando correctamente...")
+
+function bucleWhile(){
+    let count = 10;
+    let array = [];
+    while(count >= 1){
+        array.push(count);
+        count --;
+    }
+    alert(`Los numeros son: ${array.join(" - ")}`);
+}
+
+
+function bucleF(){
+    let count = 10;
+    let array = [];
+    
+
+
+
+}
