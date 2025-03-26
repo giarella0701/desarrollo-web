325aa0c855cc3baa24abd5f1d0976c37b81aee76
leslieVVH <leslievillagra@liceovvh.cl>
2025-03-26 08:50:26 -0300

    ok

--- 09_ejercicios_Condicionales/script.js
+++ 09_ejercicios_Condicionales/script.js
@@ -1,21 +1,37 @@
 console.log("Funcionando correctamente");
 
 function botonparImpar(){  //ejercicio 14
-    let numero = prompt("Ingresar Cualquier numero:")
-
-
-
+    let numero = parseInt(prompt("Ingresar cualquier numero:"));
+    if(numero % 2 == 0){
+        alert("El numero es par");
+    } else{
+        alert("El numero es impar");
+    }
 }
 
 function calculaMasa(){  //ejercicio 15
-    let peso = prompt("Ingresa tu peso (en Kg):")
-    let altura = prompt("Ingrsa tu altura (en metros):")
-
-
-
+    let peso = parseFloat(prompt("Ingresa tu peso (en Kg):"));
+    let altura = parseFloat(prompt("Ingrsa tu altura (en cm):"));
+    altura = altura / 100;
+    let imc = peso / (altura + altura);
+    if(imc < 18.5){
+        alert("Bajo peso");
+    } else if (imc >= 18.5 && imc <= 24.9){
+        alert("Peso normal")
+    }else{
+        alert("Sobrepeso")
+    } 
 }
 
 function calculaNotas(){  //ejercicio 16
-
+    let nota1 = parseFloat(prompt("Ingresa tu primera nota:"));
+    let nota2 = parseFloat(prompt("Ingresa tu segunda nota:"));
+    let nota3 = parseFloat(prompt("Ingresa tu tercera nota:"));
+    let calculaNotas = (nota1 + nota2 + nota3) / 3;
+    if(calculaNotas < 4.0){
+        alert("Reprobado");
+    } else{
+        alert("Aprovado")
+    }
 
 }
