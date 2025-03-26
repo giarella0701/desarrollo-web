8bc0a22155ed2f51b4514268efb5dad45bdc0ce7
leslieVVH <leslievillagra@liceovvh.cl>
2025-03-26 16:11:21 -0300

    ok

--- 10_Bucles/script.js
+++ 10_Bucles/script.js
@@ -10,12 +10,31 @@ function bucleWhile(){
     alert(`Los numeros son: ${array.join(" - ")}`);
 }
 
-
 function bucleF(){
-    let count = 10;
     let array = [];
-    
+    for(i = 10; i >= 1; i -- ){
+        array.push(i)
+    }
+    alert(`Los numeros son: ${array.join(" - ")}`);
+}
 
+function contarNumeros(){
+    let n = parseInt(prompt("Ingrese un numero entero:"));
+    let count = 1;
+    let array = [];
+    while(count <= n){
+        array.push(count);
+        count++
+    }
+    alert(`Contando: ${array.join(" - ")}`)
+}
 
-
+function sumarNumeros(){
+    let suma = 0;
+    let array = [];
+    for( let i = 1; 1 <= 5; i++){
+        suma+= i;
+        array.push(i);
+    }
+    alert(suma);
 }
