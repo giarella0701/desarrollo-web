0d561ffb3aadeb350ebb79144596e9576e39dc38
giarella0701 <giarellaavello@liceovvh.cl>
2025-07-21 10:16:12 -0400

    ok

--- 2.0/script.js
+++ 2.0/script.js
@@ -1,5 +1,6 @@
 const resultado = document.getElementById("resultado");
 
+
 function agregarElementoCompra() {
     let elemento = document.getElementById("lista").value;
     // Crear un elemento html --> CreateElement
@@ -9,6 +10,8 @@ function agregarElementoCompra() {
     //appendChild inserta un valor en el elemento padre "resultado"
     resultado.appendChild(item);
 }
+
+
 
 let carrito = [];
 
@@ -23,6 +26,8 @@ function mostrarListaCompra() {
         lista.appendChild(li);
     }
 }
+
+
 
 function agregarElemento2() {
     const input = document.getElementById("lista2");
@@ -43,32 +48,107 @@ function eliminarUltimoElemento() {
     mostrarListaCompra()
 }
 
+let colores = [];
 
+function mostrarListaColores() {
+    const input = document.getElementById("lista3");
+    const lista = document.getElementById("resultado3");
+    const nuevoColor = input.value.trim();
 
-let colores = []
-
-function mostrar_lista_colores(){
-    const lista = document.getElementById("Resultado3");
+    if (nuevoColor !== "") {
+        colores.unshift(nuevoColor);
+    }
     lista.innerHTML = "";
-
-    for (let i = 0; i > colores.length; i++){
+    for (let i = 0; i < colores.length; i++) {
         const li = document.createElement("li");
-        li.className = "list-group-item d-flex justify-content-between aling-items-center mb-2";
-        li.textContent = `- ${colores[i]}`;
+        li.className = "list-group-item";
+        li.textContent = colores[i];
+        lista.appendChild(li);
+    }
+    input.value = "";
+    input.focus();
+}
+
+function eliminarPrimero() {
+    const eliminado = colores.shift();
+    const lista = document.getElementById("resultado4");
+    const mensaje = document.getElementById("colorEliminado");
+
+    lista.innerHTML = "";
+    for (let i = 0; i < colores.length; i++) {
+        const li = document.createElement("li");
+        li.className = "list-group-item";
+        li.textContent = colores[i];
+        lista.appendChild(li);
+    }
+}
+function mostrarLista() {
+    const input = document.getElementById("lista4");
+    const lista = document.getElementById("resultado4");
+    const nuevoColor = input.value.trim();
+
+    if (nuevoColor !== "") {
+        colores.unshift(nuevoColor);
+    }
+    lista.innerHTML = "";
+    for (let i = 0; i < colores.length; i++) {
+        const li = document.createElement("li");
+        li.className = "list-group-item";
+        li.textContent = colores[i];
+        lista.appendChild(li);
+    }
+    input.value = "";
+    input.focus();
+}
+
+let numeros = [10, 20, 30]
+
+function mostrarListaNumeros(){
+    const lista = document.getElementById("resultado5");
+    lista.innerHTML = ""; // Limpiar antes de mostrar
+
+    for (let i = 0; i < numeros.length; i++){
+        const li = document.createElement("li")
+        li.className = "list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `${numeros[i]}`; // <li>texContent</li>
         lista.appendChild(li);
     }
 }
 
-function agregarAlInicio(){
-    const input = document.getElementById("lista3");
-    const valor = input.value.trim();
+function agregar_inicio_ej5(){  
+    const input = document.getElementById("lista5");
+    const valor = input.value.trim(); // Guarda valor sin espacios extras
 
-    if(valor === ""){
-        return;
-    }
+    if (valor === "") return;
+    numeros.unshift(valor) // Agregar valor al final
 
     input.value = "";
     input.focus();
+    mostrarListaNumeros()
+}
 
-    mostrarListaCompra()
+function agregar_ultimo_ej5(){
+    const input = document.getElementById("lista5");
+    const valor = input.value.trim(); // Guarda valor sin espacios extras
+
+    if (valor === "") return;
+    numeros.push(valor) // Agregar valor al final
+
+    input.value = "";
+    input.focus(); // Agrega foco hacia input
+    mostrarListaNumeros()
+}
+
+function eliminar_primer_ej5(){
+        if (numeros.length > 0) {
+        numeros.shift();
+    }
+    mostrarListaNumeros()
+}
+
+function eliminar_ultimo_ej5(){
+    if (numeros.length > 0) {
+    numeros.pop();
+    }
+    mostrarListaNumeros()
 }
